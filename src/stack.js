6adcdb2bdad51e761a01db29d2d8c8b1502c8535
Bramalamm <fredrik.bramell@gmail.com>
2021-05-06 14:22:55 +0200

    lade till några html, js och en bundle.js

---
+++ src/stack.js
@@ -0,0 +1,16 @@
+var _ = require('underscore');
+
+let stack = [];
+
+
+exports.push = function (x) {
+   stack.push(x);
+};
+
+exports.pop = function () {
+   return stack.pop();
+}
+
+exports.peek = function () {
+   return stack[0]; // Det här är medvetet felaktigt
+}
