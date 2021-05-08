4289007dee8d280e8b015c5689412b68f6152173
Bramalamm <fredrik.bramell@gmail.com>
2021-05-08 14:46:14 +0200

    Lägger upp felaktiga tester för inlämningsuppgiften

---
+++ tests/browser.test.js
@@ -0,0 +1,26 @@
+const { Builder, By, until } = require('selenium-webdriver');
+const { iteratee } = require('underscore');
+require('geckodriver');
+
+const fileUnderTest = 'file://' + __dirname.replace(/ /g, '%20') + '/../dist/index.html';
+const defaultTimeout = 10000;
+let driver;
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 5;
+
+// Säkerställ att Firefox är igång med asynkront anrop
+beforeAll(async () => {
+	console.log(fileUnderTest);
+		driver = await new Builder().forBrowser('firefox').build();
+		await driver.get(fileUnderTest);
+	});
+
+// Avslutar Firefox med asynkront anrop
+afterAll(async() => {
+    await driver.quit();
+}, defaultTimeout);
+
+// Själva testet
+test('The stack should be empty when popping from stack in the beginning', async () => {
+	let stack = await driver.findElement(By.id('top_of_stack')).getText();
+	expect(stack).toEqual("undefined") // Fel med flit
+});
