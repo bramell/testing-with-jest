0f31700e7eab628810810eb97832c4f1928a473d
Bramalamm <fredrik.bramell@gmail.com>
2021-05-08 14:52:47 +0200

    Lägger upp korrekta tester för inlämningsuppgiften.

--- tests/stack.test.js
+++ tests/stack.test.js
@@ -2,5 +2,5 @@ const stack = require('../src/stack');
 
 // Testar ifall stacken är undefined efter att ett element tas bort
 test('pop on stack with one element returns undefined', () => {
-    expect(stack.pop()).toBeDefined(); // Fel med flit
+    expect(stack.pop()).toBeUndefined(); // Åtgärdat, istället för toBeDefined ska det vara tobeUndefined
 });
